3db88bd8a014720e7646076006765d03f4d61874
hamza324 <hamzashujaat218@gmail.com>
2025-01-07 00:05:34 +0100

    Addin github CI

--- apps/my-express-api/src/main.ts
+++ apps/my-express-api/src/main.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to my-express-api!' });
+  res.send({ message: 'Welcome to my-express-api 2!' });
 });
 
 const port = process.env.PORT || 3333;

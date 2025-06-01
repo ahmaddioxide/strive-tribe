eefe56a597b9b7fc3752bb953a3d327de78e5566
Younas Qayyum <younas.qayyum@weatherwalay.co>
2025-06-01 17:42:16 +0500

    markAsRead Issue Resolved

--- apps/my-express-api/src/chat/infrastructure/models/Conversation.ts
+++ apps/my-express-api/src/chat/infrastructure/models/Conversation.ts
@@ -1,19 +1,21 @@
-//src/chat/infrastructure/models/Conversation.ts
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IConversation extends Document {
-    participants: string[];
-    lastMessage: string;
-    lastMessageTimestamp: Date;
-    unreadCount: number;
+  participants: string[];
+  lastMessage: string;
+  lastMessageTimestamp: Date;
+  unreadCounts: { [userId: string]: number };
 }
 
 const ConversationSchema: Schema = new Schema({
-    participants: { type: [String], required: true },
-    lastMessage: { type: String, required: true },
-    lastMessageTimestamp: { type: Date, default: Date.now },
-    unreadCount: { type: Number, default: 0 }
+  participants: { type: [String], required: true },
+  lastMessage: { type: String, required: true },
+  lastMessageTimestamp: { type: Date, default: Date.now },
+  unreadCounts: {
+    type: Map,
+    of: Number,
+    default: {},
+  },
 });
-
 
 export default mongoose.model<IConversation>("Conversation", ConversationSchema);
